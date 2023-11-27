9ba3d28fb472f35c992af98fbffb030e8eee6745
a1980dominov <a1980dominov@gmail.com>
2023-11-27 16:08:21 +0200

    I`m kaking function identity

---
+++ Users/Users/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class AdminController : Controller
+    {
+        public UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> user)
+        {
+            userManager = user;
+        }
+        public IActionResult Index() => View(userManager.Users);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModels model)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError(" ", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
